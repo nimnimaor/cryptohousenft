1434f1312eea3695bb60d2aaa3e6fe45c4755c8a
nimnimaor <nimnimaor@gmail.com>
2021-11-05 09:31:08 +0200

    update version
    update wordpress version

--- wp-config.php
+++ wp-config.php
@@ -18,9 +18,6 @@
  * @package WordPress
  */
 
-define('WP_ALLOW_REPAIR', true);
-
-
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'bitnami_wordpress' );
